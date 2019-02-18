d4306ff8904263dfbe38431ac74436dc81d5f45e
nialditanawara <31877365+nialditanawara@users.noreply.github.com>
2019-02-19 01:26:48 +0700

    Add files via upload

---
+++ PBO2-10116058-Latihan7-integer/src/pbo2/pkg10116058/latihan7/integer/PBO210116058Latihan7Integer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116058.latihan7.integer;
+
+public class PBO210116058Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b =120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.err.println("short : " + s);
+        System.out.println("int :" + i);
+        System.out.println("long :" + l);
+                
+                }
+    
+}
